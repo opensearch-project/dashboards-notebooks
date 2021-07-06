04500a753aa4295f5b5cbf2ae685fa23ee5c6b5c
Joshua Li <joshuali925@gmail.com>
2021-07-06 18:34:20 +0000

    Remove dependency on dashboards version
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-notebooks/public/components/helpers/reporting_context_menu_helper.tsx
+++ dashboards-notebooks/public/components/helpers/reporting_context_menu_helper.tsx
@@ -113,7 +113,7 @@ export const generateInContextReport = (
     {
       headers: {
         'Content-Type': 'application/json',
-        'osd-version': '1.0.0',
+        'osd-xsrf': 'true',
         accept: '*/*',
         'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6',
         pragma: 'no-cache',
