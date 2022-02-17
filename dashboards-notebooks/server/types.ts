f364a82f65b213fac1f382846921d79737888146
Joshua Li <joshuali925@gmail.com>
2022-02-17 09:45:09 -0800

    Remove everything except README since this is deprecated
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- dashboards-notebooks/server/types.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NotebooksPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NotebooksPluginStart {}
